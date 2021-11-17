6862bd5e26b164260310ed9384d6a5ab05fac75b
CharlesAtchison <Charles.Atchison@outlook.com>
2021-11-17 12:00:50 -0600

    Created/Changed the explore file to the MVP version that is based on the whole database

--- explore.py
+++ explore.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[101]:
+
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -8,22 +14,36 @@ from sklearn.model_selection import train_test_split
 from scipy import stats
 from sklearn.linear_model import LinearRegression
 
+# python display
+from IPython.display import HTML, display_html, display
+
 #Feature Selection
 from sklearn.feature_selection import SelectKBest, f_regression, RFE
+
+from prepare import *
+
+
+# In[135]:
+
 
 class Attitudes_explore():
     '''
     Performs a series of analyses and explore functions on various features in our data.
     '''
     
-    def __init__(self, df: pd.DataFrame, target: str):
+    def __init__(self, df: pd.DataFrame, target: str, subset_title: str):
         '''
         Initializes the self of the class.
         '''
         self.target = target
         
+        self.subset_title = subset_title
+        
         # Pull the categorical features from dataframe
-        self.categorical_features = df.drop(columns = ['qkey', "weight", target]).columns.to_list()
+        try: 
+            self.categorical_features = df.drop(columns = ['qkey', "weight", target]).columns.to_list()
+        except:
+            self.categorical_features = df.drop(columns= [target]).columns.to_list()
         
         # Split the dataframe
         train, validate, test = train_validate_test_split(df, target)
@@ -70,10 +90,47 @@ class Attitudes_explore():
     def plot_bar_graphs(self, n=5, saved=False):
         '''Plots the target and each variable for top 'n' results from the chi2 test
         '''
+        display(HTML(f'''<html><h1>{self.subset_title}</h1></html>'''))
         # Select the n most relevant p_vals
+        
         for col in self.chi2_df.head(n).index:
-            sns.barplot(x = self.y_train, y = self.X_train[col]).set_title(
-            label = f'Percent {self.target} by {col}')
+            plt.figure(figsize=(10, 5))
+            sns.barplot(x= self.y_train, y = self.X_train[col]).set_title(
+                label=f'{self.target.title()} vs {col.title()} Barplot')
+            plt.xticks(rotation = 90, horizontalalignment='right', fontsize = 12)
             if saved:
-                plt.savefig(f'images/{col.lower()}_bar_plot.png')
+                plt.savefig(f"images/{self.subset_title.replace(' ', '_').lower()}{col.lower()}_bar_plot.png")
             plt.show()
+            
+    def countplots(self, n=5, saved=False):
+        '''Runs the countplot method from Seaborn on the top n=5 (in terms of lowest p-val) columns
+        from the dataset, and then shows the plots. If the 'saved' flag is True, it will save the plts to the images
+        folder to be read into the README.
+        '''
+        HTML(f'''<html><h1><center>{self.subset_title}</h1></center></html>''')
+    
+
+
+# In[143]:
+
+
+def generate_and_return_obj(df: pd.DataFrame):
+    # This will drop the columns that you don't want to target yet
+    tmp = df[[col for col in df.columns if col not in [
+        'pes_val', 'is_very_pes', 'is_very_opt', 'attitude', 'avg_family']]]
+
+    # Create instance
+    o = Attitudes_explore(tmp, target='is_pes', subset_title = 'Whole dataset with top 5 lowest p-vals')
+
+    # Create accessable dict results
+    o.run_statistical_tests()
+    # Return the object that was created with the statistical tests being run
+    return o
+    
+
+
+# In[ ]:
+
+
+
+
