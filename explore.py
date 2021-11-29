85bc59a33ab5f12a5bb310be50d47491fe06caa4
Jason-Tellez <jason.michael.tellez@gmail.com>
2021-11-29 14:52:46 -0600

    function: find_k

--- explore.py
+++ explore.py
@@ -11,6 +11,9 @@ import re
 
 from sklearn.model_selection import train_test_split
 
+# Import Clustering
+from sklearn.cluster import KMeans
+
 from scipy import stats
 from sklearn.linear_model import LinearRegression
 
@@ -149,6 +152,52 @@ def generate_and_return_obj(df: pd.DataFrame):
 
 
 # In[ ]:
+
+def find_k(X_train, cluster_vars, k_range):
+    sse = []
+    for k in k_range:
+        kmeans = KMeans(n_clusters=k, random_state=123)
+
+        # X[0] is our X_train dataframe..the first dataframe in the list of dataframes stored in X. 
+        kmeans.fit(X_train[cluster_vars])
+
+        # inertia: Sum of squared distances of samples to their closest cluster center.
+        sse.append(kmeans.inertia_) 
+
+    # compute the difference from one k to the next
+    delta = [round(sse[i] - sse[i+1],0) for i in range(len(sse)-1)]
+
+    # compute the percent difference from one k to the next
+    pct_delta = [round(((sse[i] - sse[i+1])/sse[i])*100, 1) for i in range(len(sse)-1)]
+
+    # create a dataframe with all of our metrics to compare them across values of k: SSE, delta, pct_delta
+    k_comparisons_df = pd.DataFrame(dict(k=k_range[0:-1], 
+                             sse=sse[0:-1], 
+                             delta=delta, 
+                             pct_delta=pct_delta))
+
+    # plot k with inertia
+    plt.plot(k_comparisons_df.k, k_comparisons_df.sse, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('SSE')
+    plt.title('The Elbow Method to find the optimal k\nFor which k values do we see large decreases in SSE?')
+    plt.show()
+
+    # plot k with pct_delta
+    plt.plot(k_comparisons_df.k, k_comparisons_df.pct_delta, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Percent Change')
+    plt.title('For which k values are we seeing increased changes (%) in SSE?')
+    plt.show()
+
+    # plot k with delta
+    plt.plot(k_comparisons_df.k, k_comparisons_df.delta, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Absolute Change in SSE')
+    plt.title('For which k values are we seeing increased changes (absolute) in SSE?')
+    plt.show()
+
+    return k_comparisons_df
 
 
 
